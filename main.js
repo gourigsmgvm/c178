1c7165cf1fa24d891660cf4965a70bd1af47581f
gourigsmgvm <93879160+gourigsmgvm@users.noreply.github.com>
2023-08-30 19:46:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let latitude = 35,longitude = 100
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA'
+
+var map = new mapboxgl.Map({container: "map", style: "mapbox://styles/mapbox/streets-v11", center:[longitude, latitude], zoom: 16})
+
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions:{enableHighAccuracy: true}, 
+        trackUserLocation: true
+    })
+)
+
+map.addControl(new MapboxDirections({accessToken: mapboxgl.accessToken}), "top-left")
